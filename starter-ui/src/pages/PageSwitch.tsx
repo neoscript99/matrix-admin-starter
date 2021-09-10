fcdd53a78bff5369b3114aded85afeeaf5b61ce4
Neo <neoscript@gmail.com>
2021-09-10 11:54:44 +0800

    1.前端依赖升级，微信公众号登录优化

--- starter-ui/src/pages/PageSwitch.tsx
+++ starter-ui/src/pages/PageSwitch.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { Welcome } from './Welcome';
-import {
-  RoleList,
-  DeptList,
-  NoteList,
-  ParamList,
-  UserList,
-  OperatorSwitch,
-  UserRoleList,
-  UserProfile,
-} from 'matrix-ui-com';
+import { RoleList, DeptList, NoteList, ParamList, UserList, UserRoleList, UserProfile } from 'matrix-ui-com';
 import { PlanList } from './plan/PlanList';
 import { yard } from '../services';
 const { adminServices } = yard;
-const allOp: OperatorSwitch = { create: true, update: true, delete: true, view: true };
 export const PageSwitch = () => {
   return (
     <Switch>
-      <Route path={`/Role`} render={() => <RoleList services={adminServices} name="角色" />} />
-      <Route path={`/UserRole`} render={() => <UserRoleList services={adminServices} name="用户角色" />} />
-      <Route path={`/User`} render={() => <UserList services={adminServices} operatorVisible={allOp} name="用户" />} />
-      <Route path={`/Dept`}>{() => <DeptList services={adminServices} operatorVisible={allOp} name="机构" />}</Route>
+      <Route path={`/Role`} render={() => <RoleList services={adminServices} />} />
+      <Route path={`/UserRole`} render={() => <UserRoleList services={adminServices} />} />
+      <Route path={`/User`} render={() => <UserList services={adminServices} />} />
+      <Route path={`/Dept`}>{() => <DeptList services={adminServices} />}</Route>
       <Route path={`/Note`} render={() => <NoteList services={adminServices} name="通知" />} />
-      <Route path={`/Param`} render={() => <ParamList services={adminServices} name="参数" />} />
+      <Route path={`/Param`} render={() => <ParamList services={adminServices} />} />
       <Route path={`/Profile`} render={() => <UserProfile services={adminServices} />} />
       <Route path={`/InitialPlan`} component={PlanList} />
       <Route path={`/InitialApply`} component={PlanList} />
